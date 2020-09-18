a43abd86e6e93f70792a7f166056cc42a53cb27c
Michael <mnielsen.dev@gmail.com>
2020-09-17 17:15:02 -0700

    First commit

---
+++ Assets/_Core/Scripts/CamZone.cs
@@ -0,0 +1,36 @@
+﻿using Cinemachine;
+using UnityEngine;
+
+// Attach to an empty GameObject to create a zone which,
+//when entered by a player, shifts to another cinemachine virtual camera
+
+[RequireComponent(typeof(Collider))]
+public class CamZone : MonoBehaviour
+{
+
+  [SerializeField]
+  private CinemachineVirtualCamera virtualCamera = null;
+
+  void Start ()
+  {
+    virtualCamera.enabled = false;
+  }
+
+  void OnTriggerEnter (Collider other)
+  {
+    if (other.CompareTag("Player"))
+      virtualCamera.enabled = true;
+  }
+
+  void OnTriggerExit (Collider other)
+  {
+    if (other.CompareTag("Player"))
+      virtualCamera.enabled = false;
+  }
+
+  // This ensures that the collider attached to this GameObject has IsTrigger checked
+  private void OnValidate ()
+  {
+    GetComponent<Collider>().isTrigger = true;
+  }
+}
---
+++ Assets/_Core/Scripts/TiltedSpriteHingeJoint.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+// This script allows Hinge Joints to function correctly with our Sprite Tilt Hinge shader
+//which allows hinge rotatation even while a sprite is tilted (rotated on the X axis)
+public class TiltedSpriteHingeJoint : MonoBehaviour
+{
+  #region Constants
+
+  private static readonly int ANGLE = Shader.PropertyToID("_Angle");
+
+  #endregion
+
+
+  #region Inspector
+
+  [SerializeField]
+  private HingeJoint hinge = null;
+
+  [SerializeField]
+  private SpriteRenderer spriteRenderer = null;
+
+  #endregion
+
+
+  #region MonoBehaviour
+
+  private void Update ()
+  {
+    float angle = -hinge.angle;
+    spriteRenderer.material.SetFloat(ANGLE, angle);
+  }
+
+  #endregion
+}
